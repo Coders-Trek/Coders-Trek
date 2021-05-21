ee76854aefbe7048d5fc84d2cb0b33a8de595166
Abhay_Dhiman <abhaysdhimans@gmail.com>
2021-05-21 23:38:51 +0530

    Added design and responsiveness for vertical nav bar

--- static/js/main.js
+++ static/js/main.js
@@ -6,14 +6,14 @@ $(".vertical-nav").mouseover(function() {
     var i;
     for (i = 0; i < x.length; i++) {
     x[i].style.display = "block";
-    }
+        }
     for (i = 0; i < y.length; i++) {
         y[i].style.width = "200px";
+        y[i].style.background = "#092742";
         }
-
-        for (i = 0; i < y.length; i++) {
-            z[i].style.left = "17%";
-            }
+    for (i = 0; i < y.length; i++) {
+        z[i].style.left = "17%";
+        }
 });
 
 $(".vertical-nav").mouseout(function() {
@@ -24,14 +24,14 @@ $(".vertical-nav").mouseout(function() {
     var i;
     for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-    }
+        }
     for (i = 0; i < y.length; i++) {
         y[i].style.width = "50px";
+        y[i].style.background = "#191927";
         }
-
-        for (i = 0; i < y.length; i++) {
-            z[i].style.left = "5%";
-            }
+    for (i = 0; i < y.length; i++) {
+        z[i].style.left = "5%";
+        }
 }); 
 
 $("#list").click(function() {
@@ -44,23 +44,25 @@ $("#list").click(function() {
     {
     for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
-    }
+        }
     for (i = 0; i < y.length; i++) {
         y[i].style.width = "50px";
+        y[i].style.background = "#191927";
         }
-        for (i = 0; i < y.length; i++) {
-            z[i].style.left = "5%";
-            }
+    for (i = 0; i < z.length; i++) {
+        z[i].style.left = "5%";
+        }
     }
     else{
         for (i = 0; i < x.length; i++) {
             x[i].style.display = "block";
             }
-            for (i = 0; i < y.length; i++) {
-                y[i].style.width = "200px";
-                }
-                for (i = 0; i < y.length; i++) {
-                    z[i].style.left = "17%";
-                    }
+        for (i = 0; i < y.length; i++) {
+            y[i].style.width = "200px";
+            y[i].style.background = "#092742";
+            }
+        for (i = 0; i < z.length; i++) {
+            z[i].style.left = "17%";
+            }
     }
 }); 
