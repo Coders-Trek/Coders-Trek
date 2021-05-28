fb344fd9de3b47efe06ad960c8785b3dac825aab
Ayush Malik <ayushmalik779@gmail.com>
2021-05-28 18:34:47 +0530

    Added UnverifiedUser mddel and removed is_verified field from RegisterUser model

--- coders_trek/auth_app/models.py
+++ coders_trek/auth_app/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class UnverifiedUser(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
 class RegisterUser(models.Model):
     fname = models.CharField(max_length=50)
     lname = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
-    is_verified = models.BooleanField(default=False)
 
