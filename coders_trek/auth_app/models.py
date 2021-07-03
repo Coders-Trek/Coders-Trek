133b4c033713d627ae61c1839f5fadb54a9cecd7
Ayush Malik <ayushmalik779@gmail.com>
2021-07-03 16:34:45 +0530

    Added teams field

--- coders_trek/auth_app/models.py
+++ coders_trek/auth_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+from user_home.models import create_new_team_class
 # Create your models here.
 class UnverifiedUser(models.Model):
     fname = models.CharField(max_length=50)
@@ -14,5 +14,6 @@ class RegisterUser(models.Model):
     email = models.EmailField(max_length=100)
     password = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
+    teams = models.ManyToManyField(create_new_team_class)
     register_datetime = models.DateTimeField(auto_now_add=True)
     last_login_datetime = models.DateTimeField(auto_now_add=True)
