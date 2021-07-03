96ea1b22320395b05e2c113f3eea109fd97a7f3e
Ayush Malik <ayushmalik779@gmail.com>
2021-07-03 16:39:05 +0530

    teams_page and create_new_team functions added:

--- coders_trek/user_home/urls.py
+++ coders_trek/user_home/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('home_page', views.user_home , name = 'home_page'),
     path('get_dashboard_data', views.get_dashboard_data , name = 'get_dashboard_data'),
     path('delete_ongoing_tasks' , views.delete_ongoing_tasks , name = 'delete_ongoing_tasks'),
+    path('teams' , views.teams_page , name = 'teams_page'),
+    path('create_new_team' , views.create_new_team , name = 'create_new_team'),
 ]
--- coders_trek/user_home/views.py
+++ coders_trek/user_home/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
 from django.http import HttpResponse
 from modules.imp_funcs import mock_data_generator
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse # For AJAX --> JSON response will be returned for all AJAX calls
+from user_home.models import create_new_team_class
+from auth_app.models import RegisterUser
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def user_home(request):
@@ -22,3 +25,29 @@ def delete_ongoing_tasks(request):
         context = mock_data_generator()
         print(context)
         return JsonResponse({'data' : context} , status = 200)
+
+
+def teams_page(request):
+    teams = RegisterUser.objects.get(email = request.session['email']).teams.all()
+    context = {'teams' : []}
+    for team in teams:
+        context['teams'].append({'name' : team.team_name , 'image' : team.image ,  'description' : team.description})
+    return render(request , 'user_home/team.html' , context)
+
+def create_new_team(request):
+    if request.method == 'POST':
+        obj = create_new_team_class()
+        image = request.FILES['image']
+        obj.team_name = request.POST.get('team_name')
+
+        file_name = FileSystemStorage().save(image.name, image)
+        t_image = FileSystemStorage().url(file_name)
+
+        obj.image = t_image
+
+        obj.description = request.POST.get('description')
+        obj.save()
+        user = RegisterUser.objects.get(email=request.session['email'])
+        user.teams.add(obj)
+        user.save()
+        return redirect('user_home:teams_page')
