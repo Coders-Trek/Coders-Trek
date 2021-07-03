4938459fd67256d9b7e62fabccab1e24e5bd8d0e
Ayush Malik <ayushmalik779@gmail.com>
2021-07-03 16:40:00 +0530

    create_new_team_class added

--- coders_trek/user_home/models.py
+++ coders_trek/user_home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class create_new_team_class(models.Model):
+	image = models.ImageField(upload_to='uploads/team_image/',blank=False)
+	team_name = models.CharField(max_length=255)
+	description = models.CharField(max_length=255)
